9e29a2e6bf074b757a4866c239b462e6abfc1b1c
77RedX <akshaygautam4450@gmail.com>
2025-10-09 00:39:53 +0530

    Added trees.cpp

---
+++ trees.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define int long long
+#define MXI 1000000007LL
+#define vi vector<int>
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+using namespace std;
+// declaring Node of tree
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data1){
+        data=data1;
+        left=nullptr;
+        right=nullptr;
+    }  
+    Node(int data1,Node* left1,Node* right1){
+        data=data1;
+        left=left1;
+        right=right1;
+    }
+};
+// function for Breadth First Traversal, also called Level Order Traversal
+// basically poora tree ko level-wise traverse krte hai starting from root data, left data then right data level wise
+vector<vector<int>> BFS_traversal(Node* root){
+    vector<vector<int>> ans;
+    if(root==NULL) return ans;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        vector<int> l;
+        int si=q.size();
+        for(int i=0;i<si;i++){
+            if(q.front()->left!=NULL) q.push(q.front()->left);
+            if(q.front()->right!=NULL) q.push(q.front()->right);
+            l.push_back(q.front()->data);
+            q.pop();
+        }
+        ans.push_back(l);
+    }
+    return ans;
+}
+vector<vector<int>> preorder_traversal(Node* root){
+    
+}
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"im so cooked";
+    return 0;
+}
